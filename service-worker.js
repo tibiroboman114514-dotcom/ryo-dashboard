0318b8f2d245685c6fcdec5a9c503ff36e7ddbd3
tibiroboman114514-dotcom <tibirobo.man114514@gmail.com>
2026-07-25 18:18:07 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const C='sf6-lab-v1',A=['./','./index.html','./style.css','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='sf6-lab-v2';
+const A=['./','./index.html','./style.css','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
